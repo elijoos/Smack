e19ac4ddbda5ba192efbe790778c72fbdb412f86
COMPUTER <computer@MBA11.local>
2018-04-21 16:53:39 -0600

    End of Lesson 1. Pod files, bridging headers

---
+++ SmackApp/Supporting Files/SmackApp-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+//Allows xcode to work swift and objc in same project
+
+#import "SWRevealViewController.ht"
+
