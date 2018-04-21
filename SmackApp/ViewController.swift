70b2fc376516ff6f5bc123b6dc3c7899907afa0a
COMPUTER <computer@MBA11.local>
2018-04-21 12:20:02 -0600

    added print statement. Test commit

--- SmackApp/ViewController.swift
+++ SmackApp/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     print("hello")
     }
-
+    func printStatement() {
+        print("yoo")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
