c6fd948a0919f3c29332a4f4ead6ea12523092f3
COMPUTER <computer@MBA11.local>
2018-05-10 21:53:57 -0600

    Continued working on registering user, and rounded corners UI. Lesson 72

--- SmackApp/Controller/CreateAccountVC.swift
+++ SmackApp/Controller/CreateAccountVC.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+//Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func createAccountPressed(_ sender: Any) {
+        //guard statements are another way to unwrap an optional value
+        guard let email = emailTxt.text , emailTxt.text != "" else{return}
+        guard let pass = passTxt.text , passTxt.text != "" else{return}
+        
+        //calling  register user from Auth Service
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                //we just coded web request to server to api, api took info, created account object, and sent it to mongo db and saved it. (find db link in heroku)
+                print("registered user")
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
     performSegue(withIdentifier: UNWIND, sender: nil)
     }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
     
 }
---
+++ SmackApp/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  SmackApp
+//
+//  Created by COMPUTER on 5/10/18.
+//  Copyright © 2018 COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    //lets interface builder know we are making changes to radius
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
